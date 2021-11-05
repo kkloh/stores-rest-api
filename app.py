a5e21a09d30acbe930a57b06bd6a1cb91fb94528
KK <kokkuen_loh@hotmail.com>
2021-11-05 23:27:23 +0800

    Fixed db import when running the app

--- app.py
+++ app.py
@@ -6,17 +6,12 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, Items
 from resources.store import Store, StoreList
-from db import db
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
-
-@app.before_first_request
-def create_tables():
-    db.create_all()
 
 jwt = JWT(app, authenticate, identity)
 
@@ -27,5 +22,4 @@ api.add_resource(StoreList, '/stores')
 api.add_resource(UserRegister, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
     app.run(port=5000, debug=True)
